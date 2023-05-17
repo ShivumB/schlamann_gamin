76166a6c240881ce40603ba97a5e901f29c20dfd
Shivum Banerjee <73792393+ShivumB@users.noreply.github.com>
2023-05-17 11:40:44 -0500

    Added questions - finished core gameplay loop

--- sketch.js
+++ sketch.js
@@ -1,31 +1,35 @@
-let scene;
-let level;
+var scene;
+var level;
 
-let levels;
+var levels;
 
-let keys;
+var keys;
 
-let frame;
+var frame;
 
-let selectedTextOption;
-let textOptions;
+var selectedTextOption;
+var textOptions;
 
-let dialogue;
-let dialoguePage;
-let dialogueFrag;
+var prologue;
+var dialoguePage;
+var dialogueFrag;
 
 var codeEditor;
 var fileNames;
 var fileData;
 
-
 var questionY = 400;
 var showAnswers = false;
+
 var questions;
-var questionPage;
-var questionFrag;
+
 var answers;
 var selectedAnswer;
+var correctAnswers;
+var answeredCorrectly;
+var answeredQuestion;
+
+var responses;
 
 function setup() {
   createCanvas(1200, 600);
@@ -50,250 +54,285 @@ function setup() {
   selectedTextOption = 0;
   textOptions = [];
 
-  dialogue = [
-    "hey there", "the name's schlamann", "i put them in jail", "hope they don't break out!!!"
+  prologue = [
+    "hey there",
+    "the name's schlamann",
+    "i put them in jail",
+    "hope they don't break out!!!",
   ];
-  
+
   dialoguePage = 0;
   dialogueFrag = "";
-  
+
   codeEditor = new CodeEditor();
   fileNames = [
-    
     ["main.java", "pawn.java"],
-    
+
     ["main.java", "toy.java", "watergun.java"],
-    
+
     ["main.java", "animal.java", "dog.java"],
-    
+
     ["main.java", "robot.java", "vacuum.java"],
-    
+
     ["main.java", "tool.java", "hammer.java"],
-    
-    ["main.java", "panda.java", "buffPanda.java"]
-    
+
+    ["main.java", "panda.java", "buffPanda.java"],
   ];
-  
+
   fileData = [
-    
     [
       //level 0
       "public class Main {\npublic static void main(String[] args) {\nPawn player = new Pawn();\nwhile(true) {\nplayer.act();\nif(player.touches(orb)) win = true;;\n}\n}\n}",
-      
-      "public class Pawn {\npublic Pawn() {\n...\n}\nprivate void move() {\nif(pressedKeys.get(‘R’) {\nthis.x += 40;\n}\n}\n}"
-      
+
+      "public class Pawn {\npublic Pawn() {\n...\n}\nprivate void move() {\nif(pressedKeys.get(‘R’) {\nthis.x += 40;\n}\n}\n}",
     ],
-    
+
     [
       //level 1
       "public class Main {\npublic static void main(String[] args) {\nWatergun player = new Watergun();\nwhile(true) {\nplayer.act();\nif(winCondition) return;\n}\n}\n}",
-      
+
       "public class Toy extends GameObject {\npublic Toy() {\n//the super constructor takes two arguments - x and y position\nsuper(0, 200);\nthis.angle = 0;\n}\nprotected void rotate() {\nif(pressedKeys.get(‘O’) {\nangle++;\n}\nif(pressedKeys.get(‘P’) {\nangle--;\n}\n}\n}",
-      
+
       "",
     ],
-    
+
     [
       //level 2
       "",
       "",
       "",
     ],
-    
+
     [
       //level 3
       "",
       "",
       "",
     ],
-    
+
     [
       //level 4
       "",
       "",
       "",
     ],
-    
+
     [
       //level 5
       "",
       "",
       "",
-    ]
-    
+    ],
   ];
-  
+
   questions = [
-    
     ["so", "you", "beat", "the", "first", "level huh you silly little goose well you know what that's great for you and all"],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
   ];
-  
-  questionPage = 0;
-  questionFrag = 0;
-  
+
   answers = [
-    
+    ["yama", "oops", "inheritance", "object class"],
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
+
     ["", "", "", ""],
-    
-    ["", "", "", ""],
-    
   ];
-  
+
   selectedAnswer = [0, 0];
-  
+
+  correctAnswers = [0, 1, 3, 2, 1, 2];
+
+  answeredCorrectly = 0;
+  answeredQuestion = false;
+  responses = [
+    [
+      ["yo", "what's poppin", "you got it wrong!"],
+      
+      ["WHY", "I CANT BELIEVEIT", "YOU IDIOT", "YOU GOT IT RIGHT!!!"],
+    ],
+
+    [[], []],
+
+    [[], []],
+
+    [[], []],
+
+    [[], []],
+
+    [[], []],
+  ];
 }
 
 function keyPressed() {
-
+  
   keys[keyCode] = true;
 
   if (scene == "game") {
-
     //dev shortcut
     if (keyCode == 32) level = Math.min(5, level + 1);
 
     //make it prettier, handle in level object
     levels[level].handleKeyPressed();
-
-  }
-
-  else if (scene == "talk") {
-
-    if(keyCode == 38) selectedTextOption ++;
-    if(keyCode == 40) selectedTextOption --;
     
+  } else if (scene == "talk") {
+    
+    if (keyCode == 38) selectedTextOption++;
+    if (keyCode == 40) selectedTextOption--;
+
     selectedTextOption += textOptions.length;
     selectedTextOption %= textOptions.length;
 
-    if(keyCode == 13) {
-
-      if(selectedTextOption == 0) {
+    if (keyCode == 13) {
+      if (selectedTextOption == 0) {
         scene = "prologue";
         frame = 0;
-      } else if(selectedTextOption == 1) {
+      } else if (selectedTextOption == 1) {
         scene = "settings";
         textOptions = ["return"];
         selectedTextOption = 0;
       }
-
     }
-
-  }
-
-  else if(scene == "settings") {
-    if(keyCode == 13) {
+    
+  } else if (scene == "settings") {
+    
+    if (keyCode == 13) {
       scene = "talk";
       textOptions = ["talk to mr. schlamann", "options"];
       selectedTextOption = 0;
     }
-  }
-
-  else if(scene == "prologue") {
-    if(keyCode == 13) {
-      if(dialoguePage + 1 < dialogue.length) {
-
+    
+  } else if (scene == "prologue") {
+    
+    if (keyCode == 13) {
+      if (dialoguePage + 1 < prologue.length) {
+        dialoguePage++;
+        frame = 0;
+      } else {
+        scene = "game";
+      }
+    }
+    
+  } else if (scene == "questions") {
+    
+    if (keyCode == 13) {
+      if (!answeredQuestion && dialoguePage + 1 < questions.length) {
         dialoguePage ++;
         frame = 0;
+        
+      } else if (!answeredQuestion) {
+        
+        if (!showAnswers) {
+          showAnswers = true;
+        } else {
+          
+          frame = 0;
+          dialoguePage = 0;
+          answeredQuestion = true;
 
+          if (selectedAnswer[0] * 2 + selectedAnswer[1] == correctAnswers[level]) {
+            answeredCorrectly = 1;
+          } else {
+            answeredCorrectly = 0;
+          }
+          
+        }
+        
       } else {
-
-        scene = "game";
-
+        
+        if (dialoguePage + 1 < responses[level][answeredCorrectly].length) {
+          
+          dialoguePage++;
+          frame = 0;
+                    
+        } else {
+          
+          if(answeredCorrectly == 1) {
+            level++;
+          } else {
+            switch(level) {
+              case 0: levels[0] = new Level0(); break;
+              case 1: levels[1] = new Level1(); break;
+              case 2: levels[2] = new Level2(); break;
+              case 3: levels[3] = new Level3(); break;
+              case 4: levels[4] = new Level4(); break;
+              case 5: levels[5] = new Level5(); break;
+            }
+          }
+          
+          scene = "game";
+        }
+        
+        
       }
-
     }
-  }
-  
-  else if(scene == "questions") {
+
+    if (showAnswers) {
+      if (keyCode == 37 || keyCode == 39)
+        selectedAnswer[1] = (selectedAnswer[1] + 1) % 2;
+      if (keyCode == 38 || keyCode == 40)
+        selectedAnswer[0] = (selectedAnswer[0] + 1) % 2;
+    }
     
-    if(keyCode == 13) {
-      
-      if(questionPage + 1 < questions.length) {
-
-        questionPage ++;
-        frame = 0;
-
-      } else {
-        
-        showAnswers = true;
-        
-      }
-      
-    }
-
   }
-  
-  
-  
 }
 
 function keyReleased() {
-
   keys[keyCode] = false;
-
 }
 
 function mouseClicked() {
-  
-  if(scene == "game") {
-    if(mouseY < 50 && mouseX > 600) {
-        codeEditor.selectedFile = Math.max(Math.floor((mouseX - 600)/codeEditor.xSpacing), 0);
+  if (scene == "game") {
+    if (mouseY < 50 && mouseX > 600) {
+      codeEditor.selectedFile = Math.max(
+        Math.floor((mouseX - 600) / codeEditor.xSpacing),
+        0
+      );
     }
   }
-  
 }
 
 function draw() {
-
   switch (scene) {
-
     case "game":
-
       levels[level].play(keys);
 
       codeEditor.play(fileNames[level], fileData[level]);
-      
-      if(levels[level].win) {
-      
-        level++;
-      
+
+      if (levels[level].win) {
+
         frame = 0;
-        scene = "questions";  
-        questionPage = 0;
-      
-        if(level >= levels.length) scene = "epilogue";
-      
+        scene = "questions";
+        dialoguePage = 0;
+        showAnswers = false;
+        answeredQuestion = false;
+        questionY = 400;
+
+        if (level >= levels.length) scene = "epilogue";
       }
-      
+
       break;
 
     case "intro":
-
       background(0);
 
       push();
 
       if (frame <= 50) {
-
       } else if (frame > 50 && -2 * (frame - 50) > -3200) {
         translate(-2 * (frame - 50), 0);
       } else {
@@ -317,27 +356,26 @@ function draw() {
       break;
 
     case "talk":
-
       background(0);
 
       fill(255);
       rect(600, 250, 50, 100);
 
-      textSize(30);
-
-      fill(255, 0.1 * frame * frame);
-
-
+      
       for (let i = 0; i < textOptions.length; i++) {
-
         if (i == selectedTextOption) {
-
           if (frame > 100) {
+            strokeWeight(3);
+            noFill();
+            stroke(255);
             triangle(425, 450 + 50 * i, 425, 430 + 50 * i, 440, 440 + 50 * i);
           }
-
         }
-
+        
+        textSize(30);
+        fill(255, 0.1 * frame * frame);
+        strokeWeight(1);
+        stroke(0);
         text(textOptions[i], 450, 450 + 50 * i);
       }
 
@@ -345,9 +383,7 @@ function draw() {
 
       break;
 
-
-      case "prologue":
-
+    case "prologue":
       background(0);
 
       fill(255);
@@ -361,9 +397,9 @@ function draw() {
 
       rect(300, 400, 600, 150);
 
-      dialogueFrag = dialogue[dialoguePage].substring(0, frame/4);
+      dialogueFrag = prologue[dialoguePage].substring(0, frame / 4);
 
-      if(frame/4 > dialogue[dialoguePage].length + 5) {
+      if (frame / 4 > prologue[dialoguePage].length + 5) {
         triangle(860, 515, 870, 515, 865, 525);
       }
 
@@ -373,85 +409,96 @@ function draw() {
       textSize(30);
       text(dialogueFrag, 325, 450);
 
-      frame ++;
+      frame++;
 
       break;
-      
-      case "questions":
-      
+
+    case "questions":
       background(0);
 
       fill(255);
       stroke(0);
       strokeWeight(1);
+      //schlamann placeholder
       rect(600, questionY - 150, 50, 100);
 
+      //dialogue box
       noFill();
       stroke(255);
       strokeWeight(3);
-
       rect(300, questionY, 600, 150);
 
-      questionFrag = questions[level - 1][questionPage].substring(0, frame/4);
-
-      if(!showAnswers && frame/4 > questions[level - 1][questionPage].length + 5) {
+      
+      //triangle for next page of dialogue
+      if (!showAnswers && frame / 4 > questions[level][dialoguePage].length + 5) {
         triangle(860, 515, 870, 515, 865, 525);
+      } else if (answeredQuestion && frame / 4 > responses[level][answeredCorrectly][dialoguePage].length + 5) {
+        triangle(860, 315, 870, 315, 865, 325);
+      }
+
+      //generate question fragment
+
+      if (!answeredQuestion) {
+        dialogueFrag = questions[level][dialoguePage].substring(0, frame / 4);
+        
+      } else {
+        dialogueFrag = responses[level][answeredCorrectly][dialoguePage].substring(0, frame / 4);
       }
 
       fill(255);
       noStroke();
-
       textSize(30);
-      text(questionFrag, 325, questionY + 50);
 
-      if(showAnswers) {
-        
-        questionY += 0.1*(200 - questionY);
-        
-        if(Math.abs(200 - questionY) < 1) questionY = 200;
-        
-        if(questionY == 200) {
+      text(dialogueFrag, 325, questionY + 50);
+
+      if (showAnswers) {
+        questionY += 0.1 * (200 - questionY);
+
+        if (Math.abs(200 - questionY) < 1) questionY = 200;
+
+        if (questionY == 200) {
           
-
           noFill();
           stroke(255);
           strokeWeight(3);
-          
+
           rect(300, 400, 600, 150);
-          
-          text(answers[0], 300, 300);
-          text(answers[1], 300, 300);
-          text(answers[2], 300, 300);
-          text(answers[3], 300, 300);
-          
+
+          let x = selectedAnswer[1] * 325 + 345;
+          let y = selectedAnswer[0] * 60 + 442;
+
+          if (!answeredQuestion) {
+            triangle(x, y + 8, x, y - 8, x + 13, y);
+          }
+
+          noStroke();
+          fill(255);
+          text(answers[level][0], 375, 450);
+          text(answers[level][1], 700, 450);
+          text(answers[level][2], 375, 510);
+          text(answers[level][3], 700, 510);
         }
       }
-      
+
       frame ++;
-      
+
       break;
 
-      case "settings":
-
+    case "settings":
       background(0);
 
-      text("no settings for you", width/2, height/2);
+      text("no settings for you", width / 2, height / 2);
 
       for (let i = 0; i < textOptions.length; i++) {
-
         if (i == selectedTextOption) {
-
           if (frame > 100) {
             triangle(425, 450 + 50 * i, 425, 430 + 50 * i, 440, 440 + 50 * i);
           }
-
         }
 
         text(textOptions[i], 450, 450 + 50 * i);
       }
 
       break;
-
   }
-
 }
